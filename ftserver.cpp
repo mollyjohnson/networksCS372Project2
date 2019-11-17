78e2932a32fe3f9fc7b47dacf79d4b5ddddeced1
mollyjohnson <johnsmol@oregonstate.edu>
2019-11-17 17:49:36 +0000

    added few more libraries to .cpp file

--- ftserver.cpp
+++ ftserver.cpp
@@ -15,6 +15,26 @@ Last modified:
 #include <ctime>
 #include <cstdlib>
 #include <cmath>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <fcntl.h>
+#include <time.h>
+#include <pthread.h>
+#include <signal.h>
+#include <errno.h>
+#include <dirent.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <sys/ioctl.h>
+#include <stdbool.h>
+#include <arpa/inet.h>
 
 //specify things used from std instead of using standard namespace
 using std::cout;
