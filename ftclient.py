bf062455f9eb77dcd77ad822fc222d78035a19b1
Molly Johnson <johnsmol@oregonstate.edu>
2019-11-28 10:08:36 -0600

    removed print statements. removing delimiter in message ftclient receives from ftserver of directory contents is working!!

--- ftclient.py
+++ ftclient.py
@@ -156,10 +156,7 @@ def ReceiveMessageData(socketFDData, delimiter):
 	while(delimiter not in message):
 		message = connectionSocket.recv(MAX_MESSAGE_SIZE).decode()
 		if(delimiter in message):
-			print("delimiter was in the message this loop.")
-			print("message length with delimiter is: " + str(len(message)))
 			messageNoDelim = (message.replace(delimiter, ''))
-			print("message length with no delimiter is: " + str(len(messageNoDelim)))
 			dataArray.append(messageNoDelim)
 		else:
 			dataArray.append(message)
