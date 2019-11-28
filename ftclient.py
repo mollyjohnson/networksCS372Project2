9b0762afd79727fd04529a5e8e96781becff605d
Molly Johnson <johnsmol@oregonstate.edu>
2019-11-28 02:12:45 -0600

    Revert "back to single message string"
    This reverts commit be57f9a21a21cc268a35856aad49ef7a1437ed8f.
    
    going back to where it used dynamic array in ftclient instead of string

--- ftclient.py
+++ ftclient.py
@@ -146,10 +146,17 @@ def SocketStartup(dataHostAddress, dataPort):
 
 	return dataSocket
 
-def ReceiveDirContents(socketFDData):
+def ReceiveMessageData(socketFDData, delimiter):
 	connectionSocket, addr = socketFDData.accept()
-	message = connectionSocket.recv(MAX_MESSAGE_SIZE).decode()
-	return connectionSocket, addr, message
+	dataVect = []
+	message = ""
+	x = 0
+	while(delimiter not in message):
+		message = connectionSocket.recv(MAX_MESSAGE_SIZE).decode()
+		dataVect.append(message)
+		print("loop in receivemessagedata is: " + str(x))
+		x = x + 1
+	return connectionSocket, addr, dataVect
 
 #pre-conditions:
 #post-conditions:
@@ -182,11 +189,15 @@ def main():
 
 	if isValidCommand == True:
 		if(command == LIST_COMMAND):
-			connectionSocket, addr, dataMessage = ReceiveDirContents(socketFDData)
-			print(dataMessage)
+			connectionSocket, addr, dataMessage = ReceiveMessageData(socketFDData, delimiter)
+			print("THE DATA RECEIVED IS:")
+			#print(dataMessage)
+			x = 0
+			for object in dataMessage:
+				print(object)
+				print("loop is: " + str(x))
+				x = x + 1
 			connectionSocket.close()
-		else:
-			print("you entered -g <filename>")
 
 	socketFDControl.close()
 
--- ftserver.cpp
+++ ftserver.cpp
@@ -441,7 +441,7 @@ int main(int argc, char *argv[]){
 					//if the last one of the packets being sent
 					else{
 						
-						SendMessage(socketFDData, (directoryContents[k]));
+						SendMessage(socketFDData, (directoryContents[k] + delimiter));
 					}
 				}
 			}
